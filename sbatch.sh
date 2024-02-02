93be434754b7e1bab01a95e227851d5aaaf9587c
Iason Christofilakis <scur0303@int6.local.snellius.surf.nl>
2024-02-02 19:47:39 +0100

    new version of slurm script

--- sbatch.sh
+++ sbatch.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 #SBATCH --job-name=game_of_life
-#SBATCH --output=out_%j.out
-#SBATCH --error=err_%j.err
+#SBATCH --output=gol_%j.out
+#SBATCH --error=gol_%j.err
 #SBATCH --partition=thin_course
 #SBATCH --nodes=1
-#SBATCH --time=00:20  # 10 minutes time limit
+#SBATCH --ntasks=128
+#SBATCH --time=01:00:00  # minutes time limit
 
+module purge
 module load 2023
 module load MPICH/4.1.2-GCC-12.3.0
 
-# Array of processes
-processes=(1 2 4 8 16 32 64 128)
+# Array of total processes/threads
+total_values=(1 2 4 8 16 32 64 128)
 
-# Array of threads
-threads=(1 2 4 8 16)
 
-make
-
-# Loop over processes
-for p in "${processes[@]}"; do
-    # Loop over threads
-    for t in "${threads[@]}"; do
-        echo "Running with $p processes and $t threads"
-        mpirun -np $p --bind-to none ./game_of_life -omp $t
-        echo "----------------------"
+# Loop over total processes/threads
+for total in "${total_values[@]}"; do
+    echo "Testing for $total processes/threads"
+    
+    # Loop over possible combinations
+    for p in $(seq 1 $((total))); do
+        t=$((total / p))
+        
+        # Ensure the product of processes and threads equals the total
+        if [ $((p * t)) -eq $total ]; then
+            echo "Running with $p processes / $t threads"
+            export OMP_NUM_THREADS=$t
+            time mpirun -np $p --bind-to none ./game_of_life -omp $t
+            echo "-----------DONE--------------"
+        fi
     done
 done
